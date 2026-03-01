ee13f73c0a068f5ce6c608c4d30c4570d831963d
sammy <lednevdosa@gmail.com>
2026-03-01 17:29:31 +0300

    2026-03-01

--- dotfiles/fish/functions/gcp.fish
+++ dotfiles/fish/functions/gcp.fish
@@ -1,15 +1,10 @@
 function gcp
-    if test -d $argv[1]
-        set path $argv[1]
-        set msg (string join ' ' $argv[2..-1])
-        pushd $path
-        git add .
-        git commit -m "$msg"
-        git push
-        popd
+    if test (count $argv) -eq 0
+        set msg (date '+%Y-%m-%d')
     else
-        git add .
-        git commit -m (string join ' ' $argv)
-        git push
+        set msg (string join ' ' $argv)
     end
+    git add .
+    git commit -m "$msg"
+    git push
 end
